e9bb3fc98d703e10c3b2baeea57965305b27ac1c
Andreas Trawoeger <atrawog@gmail.com>
2024-04-30 15:40:09 +0200

    Add OVH-DNS

--- add_host.py
+++ add_host.py
@@ -12,10 +12,6 @@ application_key = os.getenv("APPLICATION_KEY")
 application_secret = os.getenv("APPLICATION_SECRET")
 consumer_key = os.getenv("CONSUMER_KEY")
 domain = os.getenv("DOMAIN")
-beta_host = os.getenv("BETA_HOST")
-repo_host = os.getenv("REPO_HOST")
-target = os.getenv("TARGET")
-
 # Initialize OVH client with credentials
 client = ovh.Client(
     endpoint="ovh-eu",
@@ -24,16 +20,20 @@ client = ovh.Client(
     consumer_key=consumer_key,
 )
 
-# Delete and add records for beta and repo subdomains
-delete_records(client, domain, beta_host, 'A')
-delete_records(client, domain, beta_host, 'CNAME')
-add_record(client, domain, beta_host, 'CNAME', target)
-add_record(client, domain, beta_host, 'CNAME', target)
+add_record(client, domain, 'quant-prod-quetz-4.mamba.pm.', 'A',  '162.19.52.159')
 
-delete_records(client, domain, repo_host, 'A')
-delete_records(client, domain, repo_host, 'CNAME')
-add_record(client, domain, repo_host, 'CNAME', target)
-add_record(client, domain, repo_host, 'CNAME', target)
+# add_record(client, domain, beta_host, 'CNAME', target)
+
+# Delete and add records for beta and repo subdomains
+# delete_records(client, domain, beta_host, 'A')
+# delete_records(client, domain, beta_host, 'CNAME')
+# add_record(client, domain, beta_host, 'CNAME', target)
+# add_record(client, domain, beta_host, 'CNAME', target)
+
+# delete_records(client, domain, repo_host, 'A')
+# delete_records(client, domain, repo_host, 'CNAME')
+# add_record(client, domain, repo_host, 'CNAME', target)
+# add_record(client, domain, repo_host, 'CNAME', target)
 
 # Refresh the domain's DNS zone
 endpoint = f'/domain/zone/{domain}/refresh'
---
+++ dns_utils.py
@@ -0,0 +1,15 @@
+import json
+
+def delete_records(client, domain, host, field_type):
+    endpoint = f'/domain/zone/{domain}/record'
+    ids = client.get(endpoint, fieldType=field_type, subDomain=host)
+    for id in ids:
+        record_endpoint = f'{endpoint}/{id}'
+        record_info = client.get(record_endpoint)
+        print(f"DELETE {record_endpoint}", json.dumps(record_info, indent=4))
+        client.delete(record_endpoint)
+
+def add_record(client, domain, subDomain, fieldType,target):
+    endpoint = f'/domain/zone/{domain}/record'
+    result = client.post(endpoint, fieldType=fieldType, subDomain=subDomain, target=target)
+    print(f"POST {endpoint}", json.dumps(result, indent=4))
