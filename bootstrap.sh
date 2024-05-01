ab6dff1448d0a7a39c93b1df981bb3eb2c7253e5
Andreas Trawoeger <atrawog@gmail.com>
2024-05-01 19:15:39 +0200

    Move data dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mkdir -p ./data/{test,prod}/{quetz,postgres,letsencrypt}
+if sudo -l &>/dev/null; then
+    USER_UID=$(id -u)
+    USER_GID=$(id -g)
+
+    sudo mkdir -p /data/beta-mamba-pm/{test,prod}/{quetz,postgres,letsencrypt}
+    sudo chown -R $USER_UID:$USER_GID /data/beta-mamba-pm
+fi
--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Retrieve the user's UID and GID once at the start
-USER_UID=$(id -u)
-USER_GID=$(id -g)
-
 # Check if sudo privileges are available
 if sudo -l &>/dev/null; then
     USER_UID=$(id -u)
