e54ff41a1feadb3b653512cfa519b694cc8e9ddb
Andreas Trawoeger <atrawog@gmail.com>
2024-05-05 08:00:48 +0000

    Add certbot

---
+++ certbot_ovh.sh
@@ -0,0 +1,10 @@
+certbot certonly --force-renewal  \
+    --dns-ovh --non-interactive --agree-tos \
+    -m atrawog@gmail.com --dns-ovh-credentials .secrets/certbot_ovh.ini \
+    --logs-dir . --dns-ovh-propagation-seconds 60 \
+--cert-name beta.mamba.pm \
+-d quant-prod-quetz-4.mamba.pm \
+-d quant-prod-quetz-4-repo.mamba.pm \
+-d beta.mamba.pm  \
+-d repo.mamba.pm  \
+--work-dir ./data/prod/letsencrypt --config-dir ./data/prod/letsencrypt --logs-dir .
